373aeae2a5d7a7a2ac44e299fabc0f941451c3de
1153470104 <t34_shen@163.com>
2021-02-04 21:15:47 +0800

    write test tool, make test generation function reusable

--- test/BPlusTree/BPlusTreeScratchedTest.java
+++ test/BPlusTree/BPlusTreeScratchedTest.java
@@ -11,51 +11,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BPlusTreeScratchedTest {
-    private List<BPTKey<Integer>> keyList;
-    private BPlusTree<Integer> bpt;
-
-    public void initBPT(int m) {
-        bpt = new BPlusTreeScratched<Integer>(m);
-    }
-
-    public void initKeyList(int number) {
-        keyList = new ArrayList<>();
-        for (int i = 0; i < number; i++) {
-            keyList.add(new BPTValueKey<Integer, String>(i, Integer.toString(i)));
-        }
-    }
-
-    public void addList() {
-        for(BPTKey<Integer> key: keyList){
-            bpt.addKey(key);
-        }
-    }
-
-    public void addList(List<Integer> list) {
-        for(Integer i: list) {
-            bpt.addKey(new BPTValueKey<Integer, String>(i, Integer.toString(i)));
-        }
-    }
-
-    public void makeBPT(int m, int length) {
-        initBPT(m);
-        initKeyList(length);
-        addList();
-    }
-
-    public void makeBPT(int m, List<Integer> list) {
-        initBPT(m);
-        addList(list);
-    }
-
-    public String keyListString(List<BPTKey<Integer>> kList) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("|");
-        for(BPTKey<Integer> n: kList) {
-            sb.append(" ").append(n.getKey()).append(" |");
-        }
-        return sb.toString();
-    }
+    testTool ts = new testTool();
 
     /**
      * test content
@@ -63,8 +19,8 @@ class BPlusTreeScratchedTest {
      */
     @org.junit.jupiter.api.Test
     void addKey0() {
-        makeBPT(5, 0);
-        assertEquals("| |", bpt.printBasic());
+        ts.makeBPT(5, 0);
+        assertEquals("| |", ts.bpt().printBasic());
     }
 
     /**
@@ -74,11 +30,11 @@ class BPlusTreeScratchedTest {
      */
     @org.junit.jupiter.api.Test
     void addKey1() {
-        makeBPT(5, 3);
-        assertEquals("| 0 1 2 |", bpt.printBasic());
+        ts.makeBPT(5, 3);
+        assertEquals("| 0 1 2 |", ts.bpt().printBasic());
         System.out.println();
-        makeBPT(5, 5);
-        assertEquals("| 2 |\n| 0 1 | 2 3 4 |", bpt.printBasic());
+        ts.makeBPT(5, 5);
+        assertEquals("| 2 |\n| 0 1 | 2 3 4 |", ts.bpt().printBasic());
     }
     /**
      * test content
@@ -87,11 +43,11 @@ class BPlusTreeScratchedTest {
      */
     @org.junit.jupiter.api.Test
     void addKey2() {
-        makeBPT(5, 10);
-        assertEquals("| 2 4 6 |\n| 0 1 | 2 3 | 4 5 | 6 7 8 9 |", bpt.printBasic());
+        ts.makeBPT(5, 10);
+        assertEquals("| 2 4 6 |\n| 0 1 | 2 3 | 4 5 | 6 7 8 9 |", ts.bpt().printBasic());
         System.out.println();
-        makeBPT(5, Arrays.asList(30, 7, 21, 35, 45, 1, 2, 4, 6, 9, 10, 22, 31));
-        assertEquals("| 4 7 10 30 |\n| 1 2 | 4 6 | 7 9 | 10 21 22 | 30 31 35 45 |", bpt.printBasic());
+        ts.makeBPT(5, Arrays.asList(30, 7, 21, 35, 45, 1, 2, 4, 6, 9, 10, 22, 31));
+        assertEquals("| 4 7 10 30 |\n| 1 2 | 4 6 | 7 9 | 10 21 22 | 30 31 35 45 |", ts.bpt().printBasic());
     }
 
     /**
@@ -101,29 +57,29 @@ class BPlusTreeScratchedTest {
      */
     @org.junit.jupiter.api.Test
     void addKey3() {
-        makeBPT(5, Arrays.asList(30, 7, 21, 35, 45, 1, 2, 4, 6, 9, 10, 22, 31, 36/*, 39, 49*/));
-        assertEquals("| 10 |\n| 4 7 | 30 35 |\n| 1 2 | 4 6 | 7 9 | 10 21 22 | 30 31 | 35 36 45 |", bpt.printBasic());
+        ts.makeBPT(5, Arrays.asList(30, 7, 21, 35, 45, 1, 2, 4, 6, 9, 10, 22, 31, 36/*, 39, 49*/));
+        assertEquals("| 10 |\n| 4 7 | 30 35 |\n| 1 2 | 4 6 | 7 9 | 10 21 22 | 30 31 | 35 36 45 |", ts.bpt().printBasic());
         System.out.println();
-        makeBPT(5, Arrays.asList(30, 7, 21, 35, 45, 1, 2, 4, 6, 9, 10, 22, 31, 36, 13, 15));
-        assertEquals("| 10 |\n| 4 7 | 15 30 35 |\n| 1 2 | 4 6 | 7 9 | 10 13 | 15 21 22 | 30 31 | 35 36 45 |", bpt.printBasic());
+        ts.makeBPT(5, Arrays.asList(30, 7, 21, 35, 45, 1, 2, 4, 6, 9, 10, 22, 31, 36, 13, 15));
+        assertEquals("| 10 |\n| 4 7 | 15 30 35 |\n| 1 2 | 4 6 | 7 9 | 10 13 | 15 21 22 | 30 31 | 35 36 45 |", ts.bpt().printBasic());
     }
 
     @org.junit.jupiter.api.Test
     void printData() {
         System.out.print("damn");
-        makeBPT(5, Arrays.asList(30, 7, 21, 35, 45, 1, 2, 4, 6, 9, 10, 22, 31, 36/*, 39, 49*/));
+        ts.makeBPT(5, Arrays.asList(30, 7, 21, 35, 45, 1, 2, 4, 6, 9, 10, 22, 31, 36/*, 39, 49*/));
         System.out.print("damn");
-        assertEquals("| 1 | 2 | 4 | 6 | 7 | 9 | 10 | 21 | 22 | 30 | 31 | 35 | 36 | 45 |", bpt.printData());
+        assertEquals("| 1 | 2 | 4 | 6 | 7 | 9 | 10 | 21 | 22 | 30 | 31 | 35 | 36 | 45 |", ts.bpt().printData());
 
-        makeBPT(5, Arrays.asList(30, 7, 21, 35, 45, 1, 2, 4, 6, 9, 10, 22, 31, 36, 13, 15));
-        assertEquals("| 1 | 2 | 4 | 6 | 7 | 9 | 10 | 13 | 15 | 21 | 22 | 30 | 31 | 35 | 36 | 45 |", bpt.printData());
+        ts.makeBPT(5, Arrays.asList(30, 7, 21, 35, 45, 1, 2, 4, 6, 9, 10, 22, 31, 36, 13, 15));
+        assertEquals("| 1 | 2 | 4 | 6 | 7 | 9 | 10 | 13 | 15 | 21 | 22 | 30 | 31 | 35 | 36 | 45 |", ts.bpt().printData());
     }
 
     @Test
     void search() {
-        makeBPT(5, Arrays.asList(30, 7, 21, 35, 45, 1, 2, 4, 6, 9, 10, 22, 31, 36, 13, 15));
-        assertEquals("| 10 | 13 | 15 |", keyListString(bpt.search(10, 17)));
-        assertEquals("| 9 | 10 | 13 | 15 |", keyListString(bpt.search(8, 15)));
+        ts.makeBPT(5, Arrays.asList(30, 7, 21, 35, 45, 1, 2, 4, 6, 9, 10, 22, 31, 36, 13, 15));
+        assertEquals("| 10 | 13 | 15 |", ts.keyListString(ts.bpt().search(10, 17)));
+        assertEquals("| 9 | 10 | 13 | 15 |", ts.keyListString(ts.bpt().search(8, 15)));
     }
 
 //    @org.junit.jupiter.api.Test
---
+++ test/BPlusTree/testTool.java
@@ -0,0 +1,63 @@
+package BPlusTree;
+
+import BPlusTree.BPTKey.BPTKey;
+import BPlusTree.BPTKey.BPTValueKey;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class testTool {
+    private List<BPTKey<Integer>> keyList;
+    private BPlusTree<Integer> bpt;
+
+    public testTool(){
+    }
+
+    public BPlusTree<Integer> bpt() {
+        return bpt;
+    }
+
+    public void initBPT(int m) {
+        bpt = new BPlusTreeScratched<Integer>(m);
+    }
+
+    public void initKeyList(int number) {
+        keyList = new ArrayList<>();
+        for (int i = 0; i < number; i++) {
+            keyList.add(new BPTValueKey<Integer, String>(i, Integer.toString(i)));
+        }
+    }
+
+    public void addList() {
+        for(BPTKey<Integer> key: keyList){
+            bpt.addKey(key);
+        }
+    }
+
+    public void addList(List<Integer> list) {
+        for(Integer i: list) {
+            bpt.addKey(new BPTValueKey<Integer, String>(i, Integer.toString(i)));
+        }
+    }
+
+    public void makeBPT(int m, int length) {
+        initBPT(m);
+        initKeyList(length);
+        addList();
+    }
+
+    public void makeBPT(int m, List<Integer> list) {
+        initBPT(m);
+        addList(list);
+    }
+
+    public String keyListString(List<BPTKey<Integer>> kList) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("|");
+        for(BPTKey<Integer> n: kList) {
+            sb.append(" ").append(n.getKey()).append(" |");
+        }
+        return sb.toString();
+    }
+
+}
